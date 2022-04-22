6c100666534b8a976ed86f6c025ff586013aa2b6
menzzana <henric@zazzi.se>
2022-04-22 16:41:20 +0200

    text length test

--- send.py
+++ send.py
@@ -30,7 +30,7 @@ import json
 # Constants
 #-----------------------------------------------------------------------
 # CREATE_TICKET="curl --key %s --cert %s -d \"content=%s\" https://minerva.nsc.liu.se/REST/1.0/ticket/new"
-CREATE_TICKET="curl --key %s --cert %s -d \"content=%s\" -H \"Content-Type: text/plain\" https://minerva.nsc.liu.se/REST/1.0/ticket/new"
+CREATE_TICKET="curl --key %s --cert %s -d $'content=%s' -H \"Content-Type: text/plain\" https://minerva.nsc.liu.se/REST/1.0/ticket/new"
 #-----------------------------------------------------------------------
 # Functions
 #-----------------------------------------------------------------------
