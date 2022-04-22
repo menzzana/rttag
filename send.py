e41ce29ff68c8933d9d897cf552c321ec9126ab7
menzzana <henric@zazzi.se>
2022-04-22 13:42:10 +0200

    text length test

--- send.py
+++ send.py
@@ -95,12 +95,9 @@ try:
   data['Queue'] = "General"
   if not form.getvalue('id_project').startswith('('):
     data['CF.{Project}'] = form.getvalue('id_project')
-  if form.getvalue('id_mail'):
-    data['Requestor'] = form.getvalue('id_mail')
-  if form.getvalue('id_summary'):
-    data['Subject'] = form.getvalue('id_summary')
-  if form.getvalue('id_description');
-    data['Text'] = form.getvalue('id_description')
+  data['Requestor'] = form.getvalue('id_mail')
+  data['Subject'] = form.getvalue('id_summary')
+  data['Text'] = form.getvalue('id_description')
   data['CF.{Keywords}']=""
   if not form.getvalue('id_problem_type').startswith('('):
     data['CF.{Keywords}'] += str("problem_type=" + form.getvalue('id_problem_type'))
@@ -120,11 +117,11 @@ try:
     "/var/www/cgi-bin/rttag/ssl/robot-cert.pem",
     indata)).read()
 
-  #print(indata)
-  #print("-----")
-  #print(output)
+  print(indata)
+  print("-----")
+  print(output)
   
-  print(template('redirect.tpl',text=output))  
+  #print(template('redirect.tpl',text=output))  
   out_array=output.lower().split(" ")
   ticketn=out_array[out_array.index("ticket")+1]
 
